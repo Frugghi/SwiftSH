d4668633b60ccc96835f4df26aa7e87b73d6e6d9
Tommaso Madonia <tommaso@madonia.me>
2018-03-24 18:45:57 +0000

    Move example to a separate project

--- SwiftSH/Session.swift
+++ SwiftSH/Session.swift
@@ -169,7 +169,7 @@ open class SSHSession<T: RawLibrary> {
                 }
 
                 // Try to connect to resolved address
-                if CFSocketConnectToAddress(socket, dataAddress as CFData!, Double(self.timeout)/1000) == .success {
+                if CFSocketConnectToAddress(socket, dataAddress as CFData, Double(self.timeout)/1000) == .success {
                     self.log.info("Connection to \(ipAddress) on port \(self.port) successful")
                     self.socket = socket
                     break
