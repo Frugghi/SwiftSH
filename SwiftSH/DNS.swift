b81ac0f3fb8e103087b4d4500decc7cd4c299c28
Thomas Willson <thomas.willson@me.com>
2021-01-10 12:28:18 -0800

    Update project.

--- SwiftSH/DNS.swift
+++ SwiftSH/DNS.swift
@@ -50,7 +50,6 @@ internal class DNS {
             throw DNSError.inProgress
         }
 
-        var mutableSelf = self
         var context = CFHostClientContext()
         context.info = Unmanaged.passRetained(self).toOpaque()
         CFHostSetClient(self.host, {
